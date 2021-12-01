d4140aced35ac288ac028ffb9110c7db77ab82f0
Laura Blanch <laura.blancho@gmail.com>
2021-12-01 17:15:07 +0200

    [front] Fixed issue of not redirecting to /login page after a 401 error. Moved error handling logic to a function. Added two cypress test cases for dashboard.

--- front/cypress/integration/dashboard.spec.js
+++ front/cypress/integration/dashboard.spec.js
@@ -40,5 +40,67 @@ describe('Dashboard', () => {
         cy.get('ul[name="info-0-0"]').should('contain', latestTime.toLocaleTimeString())
       })
     })
+
+    it('When scrolling, further recordings are fetched and displayed', () => {
+      const latestDate = new Date(recordingsJSON[1].recording.mediaDate)
+      const latestTime = new Date(recordingsJSON[1].recording.mediaDate)
+      latestTime.setUTCMinutes(latestTime.getUTCMinutes() + ((recordingsJSON[1].count-1) * 10))
+
+      cy.intercept({
+        method: 'GET',
+        url: '/api/recordings/**',
+      }).as('recordingsFetch')
+
+      cy.loginAdmin()
+
+      cy.visit('/dashboard')
+
+      cy.wait('@recordingsFetch')
+
+      cy.scrollTo('bottom')
+
+      cy.wait('@recordingsFetch')
+
+      cy.get('div[name="recordings-1"]').should('be.visible')
+
+      cy.get('div[name="recording-1-0"]').should('be.visible')
+
+      cy.get('div[name="recordings-1"]').should('contain', latestDate.toLocaleDateString())
+
+      cy.get('div[name="recording-1-0"]').should('contain', latestTime.toLocaleTimeString())
+
+      cy.get(`div[name="recording-1-${recordingsJSON[0].count - 1}"]`).should('contain', latestDate.toLocaleTimeString())
+
+      cy.get('div[name="recording-1-0"]').within(() => {
+        cy.get('img[name="thumbnail-1-0"]').should('have.attr', 'src', Cypress.env('mediaThumbnail'))
+        cy.get('ul[name="info-1-0"]').should('contain', 'Date')
+        cy.get('ul[name="info-1-0"]').should('contain', 'Time')
+        cy.get('ul[name="info-1-0"]').should('contain', new Date(recordingsJSON[1].recording.mediaDate).toLocaleDateString())
+        cy.get('ul[name="info-1-0"]').should('contain', latestTime.toLocaleTimeString())
+      })
+    })
+
+    it('When scrolling, but session has expired, user is sent back to /login', () => {
+      cy.intercept({
+        method: 'GET',
+        url: '/api/recordings/**',
+      }).as('recordingsFetch')
+
+      cy.loginAdmin()
+
+      cy.visit('/dashboard')
+
+      cy.wait('@recordingsFetch')
+
+      cy.get('div[name="recordings-0"]').should('be.visible')
+
+      cy.clearCookies()
+
+      cy.scrollTo('bottom')
+
+      cy.wait('@recordingsFetch')
+
+      cy.url().should('include', 'login')
+    })
   })
 })
