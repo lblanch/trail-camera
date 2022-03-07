887dde8a0ba7cb44298bdf27949d9c4fc791f546
Laura Blanch <laura.blancho@gmail.com>
2022-03-07 18:30:04 +0200

    [front] Configured testing-library's eslint Aggressive Import Reporting to be off, to avoid false positives with Cypress cy.wait.

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,6 +39,11 @@ module.exports = {
     'react/prop-types': 0
   },
   'settings': {
+    // This line is to avoid the false positive for cypress "cy.wait" reported as error:
+    // "Promise returned from `wait` must be handled  testing-library/await-async-util"
+    // This sets off the "Aggressive Imports Reporting" mechanism, so ony testing-library is checked.
+    // For further info, see https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/migration-guides/v4.md#testing-libraryutils-module
+    'testing-library/utils-module': 'off',
     'react': {
       'version': 'detect'
     }
